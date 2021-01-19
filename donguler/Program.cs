8c74ab1305ee79796074b26bdd926d222afde6e6
onur engun <onurengun@outlook.com>
2021-01-19 22:04:29 +0300

    Add project files.

---
+++ donguler/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region while
+            //int sayi = 1;
+            //while (sayi<=20)
+            //{
+            //    Console.WriteLine(sayi);
+            //    sayi++;
+            //}
+            // -başlangıç -koşul -artış miktarı olmalıdır.
+            #endregion
+
+            #region  0-100 arası sayılar
+
+            //int sayi = 0;
+            //while (sayi<=100)
+            //{
+            //    Console.WriteLine(sayi);
+            //    sayi++;
+            //}
+
+            #endregion
+
+            #region 100-0 arası sayılar
+
+            //int sayi = 100;
+            //while(sayi>=0)
+            //{
+            //    Console.WriteLine(sayi);
+            //    sayi--;
+            //}
+
+            #endregion
+
+            #region 0-100 arasındaki çift sayılar
+
+            //int sayi = 1;
+
+            //while (sayi<=100)
+            //{
+            //    if (sayi % 2 == 0)
+            //    {
+            //        Console.WriteLine(sayi);
+            //    }
+            //    sayi ++;
+            //}
+
+            #endregion
+
+            #region 0-100 arasındaki 4 e bölünen sayılar
+
+            //int sayi = 0;
+            //while(sayi<=100)
+            //{
+            //    if(sayi % 4 ==0)
+            //    {
+            //        Console.WriteLine(sayi);
+            //    }
+            //    sayi++;
+            //}
+
+            #endregion
+
+            #region 0-100 arasında 4 bölünebilen kaç sayı var?
+
+            //int sayi = 0;
+            //int sayac = 0; 
+            //while (sayi <= 100)
+            //{
+            //    if (sayi % 4 == 0)
+            //    {
+            //        sayac++;
+            //    }
+            //    sayi++;
+            //}
+            //Console.WriteLine("Sayı adeti:" + sayac);
+
+            #endregion
+
+
+        }
+    }
+}
